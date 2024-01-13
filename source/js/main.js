9a68b6a84132f93f91626daca338a15bb2be7427
NadezhdaObsholova <obsholova.n@gmail.com>
2024-01-14 01:40:32 +0300

    start hero

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,30 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
+//import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+import './vendor/swiper';
+import {initHeroSlider} from './modules/slider/hero-slider';
 
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+ // iosVhFix();
+
+  // Modules
+  // ---------------------------------
+
+  initHeroSlider();
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  /*
+  window.addEventListener('load', () => {
+    const form = new Form();
+    window.form = form;
+    form.init();
+  });
+  */
+});
---
+++ source/js/modules/slider/hero-slider.js
@@ -0,0 +1,24 @@
+
+import Swiper from '../../vendor/swiper';
+
+const initHeroSlider = () => {
+
+  const heroSlider = document.querySelector('[data-swiper="hero-slider"]');
+  const heroPagination = document.querySelector('[data-pagination="hero-pagination"]');
+
+  if (heroSlider) {
+    // eslint-disable-next-line
+    new Swiper(heroSlider, {
+
+      pagination: {
+        el: heroPagination,
+        clickable: true,
+      },
+
+      loop: true,
+    });
+  }
+};
+
+
+export { initHeroSlider };
