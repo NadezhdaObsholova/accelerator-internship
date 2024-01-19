b6ba48db7786014056cbac65f98c566b9d2a7408
NadezhdaObsholova <obsholova.n@gmail.com>
2024-01-20 00:46:01 +0300

    header + hero are done

--- source/js/modules/burger-menu.js
+++ source/js/modules/burger-menu.js
@@ -1,23 +1,52 @@
 
 const burgerMenu = function () {
+  const bodyElement = document.querySelector('body');
+  const bodyOverlayElement = document.querySelector('[data-overlay="body-overlay"]');
   const menuElement = document.querySelector('[data-menu="header-menu"]');
   const menuToggleOpen = document.querySelector('[data-open="toggle-open"]');
   const menuToggleClose = document.querySelector('[data-close="toggle-close"]');
-  const menuButtonElement = document.querySelector('[data-toggle="menu__toggle"]');//кнопка
-  const bodyElement = document.querySelector('body');
-  const bodyOverlayElement = document.querySelector('[data-overlay="body-overlay"]');
+  const menuButtonElement = document.querySelector('[data-toggle="menu__toggle"]');
+  const menuItems = document.querySelectorAll('[data-menu_item="menu_item"]');
+  const isEscapeKey = (evt) => evt.key === 'Escape';
 
-  const toggleMenu = () => {
+  function toggleMenu() {
     menuButtonElement.classList.toggle('is_open');
     menuElement.classList.toggle('visually-hidden');
-    bodyElement.classList.toggle('menuToggleClose');
+    bodyElement.classList.toggle('scroll-lock');
     menuToggleOpen.classList.toggle('visually-hidden');
     menuToggleClose.classList.toggle('visually-hidden');
     bodyOverlayElement.classList.toggle('visually-hidden');
+  }
+
+  const onDocumentKeydown = (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      closeMenu();
+    }
   };
 
-  menuButtonElement.addEventListener('click', () => {
+  function openMenu() {
     toggleMenu();
+    document.addEventListener('keydown', onDocumentKeydown);
+  }
+
+  function closeMenu() {
+    toggleMenu();
+    document.removeEventListener('keydown', onDocumentKeydown);
+  }
+
+  menuButtonElement.addEventListener('click', () => {
+    openMenu();
+  });
+
+  const onMenuItemsClick = (evt) => {
+    if (evt) {
+      closeMenu();
+    }
+  };
+
+  menuItems.forEach((item) => {
+    item.addEventListener('click', onMenuItemsClick);
   });
 
 };
